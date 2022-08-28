136f93a5007913926f9173e62f0210ff9167954f
yyxi <178086254+yyxi@users.noreply.github.com>
2022-08-28 15:07:00 +0000

    feat: initial commit

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import {
+  preflights as presetWindPreflights,
+  rules as presetWindRules,
+  shortcuts as presetWindShortcuts,
+  theme as presetWindTheme,
+  variants as presetWindVariants
+} from '@unocss/preset-wind'
+import { differenceWith } from 'lodash-es'
+import { EXCLUDE_RULES, EXCLUDE_VARIANTS } from './constants'
+import { Options, Preset } from './types'
+import { ruleComparator } from './utilities/rule-comparator'
+import { variantComparator } from './utilities/variant-comparator'
+
+export const presetEscapace = (options: Options = {}): Preset => {
+  options.dark = options.dark ?? 'class'
+  options.attributifyPseudo = options.attributifyPseudo ?? false
+
+  const rules = differenceWith(presetWindRules, EXCLUDE_RULES, ruleComparator)
+  const variants = differenceWith(
+    [...presetWindVariants(options)],
+    EXCLUDE_VARIANTS,
+    variantComparator
+  )
+
+  return {
+    name: '@escapace/unocss-preset-escapace',
+    theme: presetWindTheme,
+    rules,
+    shortcuts: presetWindShortcuts,
+    variants,
+    options,
+    preflights: presetWindPreflights,
+    prefix: options.prefix
+  }
+}
---
+++ src/utilities/rule-comparator.ts
@@ -0,0 +1,18 @@
+import type { Rule } from '@unocss/core'
+import { isEqualWith, isRegExp } from 'lodash-es'
+
+export const ruleComparator = (rule: Rule, exclude: string | RegExp) => {
+  const matcher = rule[0]
+
+  return isEqualWith(
+    matcher,
+    exclude,
+    (a: RegExp | string, b: RegExp | string) => {
+      if (isRegExp(a) && isRegExp(b)) {
+        return a.source === b.source
+      }
+
+      return a === b
+    }
+  )
+}
