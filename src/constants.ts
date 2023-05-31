88f2c47bf04b99fffc75500be9933f626374bcf8
Mark Martirosian <hello@markmartirosian.com>
2023-05-31 16:19:24 +0000

    fix: workflow

--- src/constants.ts
+++ src/constants.ts
@@ -28,83 +28,63 @@ export const EXCLUDE_RULES: Array<string | RegExp> = [
   /^(?:border|b)-(block|inline)-(?:color-)?(.+)$/,
   /^(?:border|b)-([bi][se])-(?:color-)?(.+)$/,
 
-  /^(?:border|b)-()op(?:acity)?-?(.+)$/,
-  /^(?:border|b)-([xy])-op(?:acity)?-?(.+)$/,
-  /^(?:border|b)-([rltbse])-op(?:acity)?-?(.+)$/,
-  /^(?:border|b)-(block|inline)-op(?:acity)?-?(.+)$/,
-  /^(?:border|b)-([bi][se])-op(?:acity)?-?(.+)$/,
-
   // https://github.com/unocss/unocss/blob/main/packages/preset-mini/src/_rules/color.ts
   /^(?:color|c)-(.+)$/,
   /^text-(.+)$/,
   /^bg-(.+)$/,
+  /^(?:text|color|c)-(.+)$/,
   /^(?:text|color|c)-op(?:acity)?-?(.+)$/,
   /^bg-op(?:acity)?-?(.+)$/,
 
   // https://github.com/unocss/unocss/blob/main/packages/preset-mini/src/_rules/decoration.ts
   /^(?:underline|decoration)-(.+)$/,
-  /^(?:underline|decoration)-op(?:acity)?-?(.+)$/,
 
   // https://github.com/unocss/unocss/blob/main/packages/preset-mini/src/_rules/ring.ts
 
   /^ring-(.+)$/,
-  /^ring-op(?:acity)?-?(.+)$/,
   /^ring-offset-(.+)$/,
-  /^ring-offset-op(?:acity)?-?(.+)$/,
 
   // https://github.com/unocss/unocss/blob/main/packages/preset-mini/src/_rules/shadow.ts
 
   /^shadow(?:-(.+))?$/,
-  /^shadow-(.+)$/,
-  /^shadow-op(?:acity)?-?(.+)$/,
-  'shadow-inset',
 
   // https://github.com/unocss/unocss/blob/main/packages/preset-mini/src/_rules/svg.ts
 
   /^fill-(.+)$/,
-  /^fill-op(?:acity)?-?(.+)$/,
   /^stroke-(.+)$/,
-  /^stroke-op(?:acity)?-?(.+)$/,
 
   // https://github.com/unocss/unocss/blob/main/packages/preset-mini/src/_rules/typography.ts
 
   /^text-stroke-(.+)$/,
-  /^text-stroke-op(?:acity)?-?(.+)$/,
-  /^text-shadow(?:-(.+))?$/,
   /^text-shadow-color-(.+)$/,
-  /^text-shadow-color-op(?:acity)?-?(.+)$/,
 
   // https://github.com/unocss/unocss/blob/main/packages/preset-wind/src/rules/background.ts
-  /^bg-gradient-(.+)$/,
-  /^(?:bg-gradient-)?stops-(\[.+\])$/,
-  /^(?:bg-gradient-)?from-(.+)$/,
-  /^(?:bg-gradient-)?via-(.+)$/,
-  /^(?:bg-gradient-)?to-(.+)$/,
-  /^(?:bg-gradient-)?from-op(?:acity)?-?(.+)$/,
-  /^(?:bg-gradient-)?via-op(?:acity)?-?(.+)$/,
-  /^(?:bg-gradient-)?to-op(?:acity)?-?(.+)$/,
-  /^bg-gradient-((?:repeating-)?(?:linear|radial|conic))$/,
-  /^bg-gradient-to-([rltb]{1,2})$/,
-  /^(?:bg-gradient-)?shape-(.+)$/,
+  // /^bg-gradient-(.+)$/,
+  /^(?:bg-gradient-)?(from)-(.+)$/,
+  /^(?:bg-gradient-)?(via)-(.+)$/,
+  /^(?:bg-gradient-)?(to)-(.+)$/,
+  // /^(?:bg-gradient-)?(from|via|to)-op(?:acity)?-?(.+)$/,
 
   // https://github.com/unocss/unocss/blob/main/packages/preset-wind/src/rules/behaviors.ts
   /^accent-(.+)$/,
-  /^accent-op(?:acity)?-?(.+)$/,
   /^caret-(.+)$/,
-  /^caret-op(?:acity)?-?(.+)$/,
 
   // https://github.com/unocss/unocss/blob/main/packages/preset-wind/src/rules/divide.ts
   /^divide-(.+)$/,
-  /^divide-op(?:acity)?-?(.+)$/,
+  // /^divide-op(?:acity)?-?(.+)$/,
 
   // https://github.com/unocss/unocss/blob/main/packages/preset-wind/src/rules/filters.ts
-  /^(?:filter-)?drop-shadow(?:-(.+))?$/,
   /^(?:filter-)?drop-shadow-color-(.+)$/,
-  /^(?:filter-)?drop-shadow-color-op(?:acity)?-?(.+)$/,
+  // /^(?:filter-)?drop-shadow(?:-(.+))?$/,
+  // /^(?:filter-)?drop-shadow-color-(.+)$/,
+  // /^(?:filter-)?drop-shadow-color-op(?:acity)?-?(.+)$/,
 
   // https://github.com/unocss/unocss/blob/main/packages/preset-mini/src/_rules/border.ts
   /^(?:border|b)-()(?:color-)?(.+)$/,
   /^(?:border|b)-([xy])-(?:color-)?(.+)$/,
+  /^(?:border|b)-([rltbse])-(?:color-)?(.+)$/,
+  /^(?:border|b)-(block|inline)-(?:color-)?(.+)$/,
+  /^(?:border|b)-([bi][se])-(?:color-)?(.+)$/,
 
   // https://github.com/unocss/unocss/blob/main/packages/preset-wind/src/rules/placeholder.ts
   /^\$ placeholder-(.+)$/,
